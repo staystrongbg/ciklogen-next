7fffac0951403517c767f8df1f57e42d7c013c2c
ddamaja <79502735+staystrongbg@users.noreply.github.com>
2021-12-19 08:58:14 +0100

    Delete components directory

--- components/author.js
+++
@@ -1,8 +0,0 @@
-import styles from '../styles/story.module.css';
-export const Author = ({ autor }) => {
-  return (
-    <div className={styles['story-text']}>
-      <span className={styles.author}>{autor}</span>
-    </div>
-  );
-};
--- components/intro.js
+++
@@ -1,8 +0,0 @@
-import styles from '../styles/story.module.css';
-export const Intro = ({ intro }) => {
-  return (
-    <div className={styles.intro}>
-      <p>{intro}</p>
-    </div>
-  );
-};
